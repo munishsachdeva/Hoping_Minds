74290ecab7885bc8e75202173e608a7112095cd2
Munish <45123618+munishsachdeva@users.noreply.github.com>
2023-02-06 21:49:21 +0530

    using only one loop

--- Hoping_Minds_06Feb2023_MunishSachdeva/06Feb2023_FSDB01_MunishSachdeva-1.py
+++ Hoping_Minds_06Feb2023_MunishSachdeva/06Feb2023_FSDB01_MunishSachdeva-1.py
@@ -1,27 +1,28 @@
-
 # arr.sort()
 # print(arr)
+
 def bubble(arr):
-    n = len(arr)
-    for i in range(n-1):
-        for j in range(n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-                # temp = arr[j]
-                # arr[j] = arr[j+1]
-                # arr[j+1] = temp
-                # j = j+1
-            print(f'step {i+1} {arr}')
-            #return(arr)
-    print()
-    return()
+    has_swapped = True
+    while(has_swapped):
+        has_swapped = False
+        n = len(arr)
+        for i in range(n-1):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                has_swapped = True
+            print(f'step {i} {arr}')
+        print()
+                # temp = arr[i]
+                # arr[i] = arr[i+1]
+                # arr[i+1] = temp
+                # i = i+1
+    return arr
 
 arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 arr2 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 arr3 = [5, 6, 4, 3, 1, 2, 7, 8, 9, 10]
 
-bubble(arr1)
-bubble(arr2)
-bubble(arr3)
-
+print(bubble(arr1))
+print(bubble(arr2))
+print(bubble(arr3))
 
